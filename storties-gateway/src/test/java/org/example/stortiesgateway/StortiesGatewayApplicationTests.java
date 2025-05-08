29baa736ece49854dab91aeb9048c58fb8bb18ac
김경민 <kkm07133@naver.com>
2025-05-08 12:11:16 +0900

    feat: (#2) spring 기본 세팅 추가

---
+++ storties-gateway/src/test/java/org/example/stortiesgateway/StortiesGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stortiesgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StortiesGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
